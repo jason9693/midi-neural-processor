b310b2595601d1361e0b782e572ddec79b003138
Yang <ykcha9@gmail.com>
2019-07-16 09:24:04 +0900

    initial commit

---
+++ processor.py
@@ -0,0 +1,166 @@
+import pretty_midi
+
+
+RANGE_NOTE_ON = 128
+RANGE_NOTE_OFF = 128
+RANGE_VEL = 32
+RANGE_TIME_SHIFT = 100
+
+START_IDX = {
+    'note_on': 0,
+    'note_off': RANGE_NOTE_ON,
+    'time_shift': RANGE_NOTE_ON + RANGE_NOTE_OFF,
+    'velocity': RANGE_NOTE_ON + RANGE_NOTE_OFF + RANGE_TIME_SHIFT
+}
+
+
+# Divided note by note_on, note_off
+class SplitNote:
+    def __init__(self, type, time, value, velocity):
+        ## type: note_on, note_off
+        self.type = type
+        self.time = time
+        self.velocity = velocity
+        self.value = value
+
+    def __repr__(self):
+        return '<[SNote] time: {} type: {}, value: {}, velocity: {}>'\
+            .format(self.time, self.type, self.value, self.velocity)
+
+
+class Event:
+    def __init__(self, event_type, value):
+        self.type = event_type
+        self.value = value
+
+    def __repr__(self):
+        return '<Event type: {}, value: {}>'.format(self.type, self.value)
+
+    def to_int(self):
+        return START_IDX[self.type] + self.value
+
+    @staticmethod
+    def from_int(int_value):
+        info = Event._type_check(int_value)
+        return Event(info['type'], info['value'])
+
+    @staticmethod
+    def _type_check(int_value):
+        range_note_on = range(0, RANGE_NOTE_ON)
+        range_note_off = range(RANGE_NOTE_ON, RANGE_NOTE_ON+RANGE_NOTE_OFF)
+        range_time_shift = range(RANGE_NOTE_ON+RANGE_NOTE_OFF,RANGE_NOTE_ON+RANGE_NOTE_OFF+RANGE_TIME_SHIFT)
+
+        valid_value = int_value
+
+        if int_value in range_note_on:
+            return {'type': 'note_on', 'value': valid_value}
+        elif int_value in range_note_off:
+            valid_value -= RANGE_NOTE_ON
+            return {'type': 'note_off', 'value': valid_value}
+        elif int_value in range_time_shift:
+            valid_value -= (RANGE_NOTE_ON + RANGE_NOTE_OFF)
+            return {'type': 'time_shift', 'value': valid_value}
+        else:
+            valid_value -= (RANGE_NOTE_ON + RANGE_NOTE_OFF + RANGE_TIME_SHIFT)
+            return {'type': 'velocity', 'value': valid_value}
+
+
+def _divide_note(mid_path):
+    result_array = []
+    notes = []
+    mid = pretty_midi.PrettyMIDI(midi_file=mid_path)
+
+    for inst in mid.instruments:
+        notes += inst.notes
+
+    notes.sort(key=lambda x: x.start)
+    for note in notes:
+        on = SplitNote('note_on', note.start, note.pitch, note.velocity)
+        off = SplitNote('note_off', note.end, note.pitch, None)
+        result_array += [on, off]
+    return result_array
+
+
+def _merge_note(snote_sequence):
+    note_on_queue = [snote for snote in snote_sequence if snote.type == 'note_on']
+    note_off_queue = []
+    # for snote in snote_sequence:
+
+
+
+def _snote2events(snote: SplitNote, prev_vel: int):
+    result = []
+    if snote.velocity is not None:
+        modified_velocity = snote.velocity // 4
+        if prev_vel != modified_velocity:
+            result.append(Event(event_type='velocity', value=modified_velocity))
+    result.append(Event(event_type=snote.type, value=snote.value))
+    return result
+
+
+def _event_seq2snote_seq(event_sequence):
+    timeline = 0
+    velocity = 0
+    snote_seq = []
+
+    for event in event_sequence:
+        if event.type == 'time_shift':
+            timeline += (event.value / 100)
+        if event.type == 'velocity':
+            velocity = event.value * 4
+        else:
+            snote = SplitNote(event.type, timeline, event.value, velocity)
+            snote_seq.append(snote)
+    return snote_seq
+
+
+def _make_time_sift_events(prev_time, post_time):
+    time_interval = int((post_time - prev_time) * 100)
+    results = []
+    while time_interval >= RANGE_TIME_SHIFT:
+        results.append(Event(event_type='time_shift', value=RANGE_TIME_SHIFT-1))
+        time_interval -= RANGE_TIME_SHIFT
+    if time_interval == 0:
+        return results
+    else:
+        return results + [Event(event_type='time_shift', value=time_interval-1)]
+
+
+def encode_midi(file_path):
+    events = []
+    dnotes = _divide_note(file_path)
+    dnotes.sort(key=lambda x: x.time)
+
+    cur_time = 0
+    cur_vel = 0
+    for snote in dnotes:
+        events += _snote2events(snote=snote, prev_vel=cur_vel)
+        events += _make_time_sift_events(prev_time=cur_time, post_time=snote.time)
+
+        cur_time = snote.time
+        cur_vel = snote.velocity
+    # print([(e,e.to_int()) for e in events])
+    return [e.to_int() for e in events]
+
+
+# TODO: add decode code
+def decode_midi(idx_array):
+    event_sequence = [Event.from_int(idx) for idx in idx_array]
+    snote_seq = _event_seq2snote_seq(event_sequence)
+
+
+    pass
+
+
+if __name__ == '__main__':
+    print(encode_midi('bin/ADIG04.mid'))
+
+# ins = pretty_midi.PrettyMIDI('bin/ADIG04.mid')
+# print(ins)
+#
+# for i in ins.instruments:
+#     notes = i.notes
+#     notes.sort(key=lambda x: x.start)
+#     for j in notes:
+#        print('start:{} end: {}, pitch: {}, vel: {}'.format(j.start, j.end, j.pitch, j.velocity//4))
+
